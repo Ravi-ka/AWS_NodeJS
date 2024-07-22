4a1f7859ed0446663a0b00d1b99b0637f2131b19
Ravisankar.Ka <ka.ravisankar@gmail.com>
2024-07-22 23:03:19 +0530

    IAM Attach Policy To User - V3

---
+++ src/IAM_V3/attachPoliciesToUser.js
@@ -0,0 +1,23 @@
+import { IAMClient, AttachUserPolicyCommand } from "@aws-sdk/client-iam";
+
+const iamClient = new IAMClient();
+const configParams = {
+  PolicyArn: "arn:aws:iam::533267138132:policy/S3-Read-Only",
+  UserName: "Admin-User-2",
+};
+
+const executeFunction = async () => {
+  try {
+    const data = await iamClient.send(
+      new AttachUserPolicyCommand(configParams)
+    );
+    console.log(
+      `Policy(${configParams.PolicyArn}) added to ${configParams.UserName} `
+    );
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+executeFunction();
